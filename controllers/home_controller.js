bccf82e58757fa9661b561aaf6406398b99b5be8
RaghavendraPatel <raghavpatel2808@hotmail.com>
2023-05-04 07:52:02 +0530

    added posts to home and populated posts with user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,15 @@
+const Post = require('../models/post');
+const User = require('../models/user');
 module.exports.home = (req,res)=>{
-    return res.render('home',{
-        title:'Home',
-    });
+
+    Post.find({}).populate('user').exec()
+    .then((posts)=>{
+        return res.render('home',{
+            title:'Codial | Home',
+            posts
+        });
+    })
+    .catch((err)=>{
+        console.log('Error fetching posts');
+    })
 }
