6a2b0e915e3eca7ad11f45d38b5fb2273be1010c
RaghavendraPatel <raghavpatel2808@hotmail.com>
2023-05-12 18:48:36 +0530

    converted to async await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,34 +1,30 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post')
-module.exports.create = (req,res)=>{
-    Post.findById(req.body.post)
-    .then((post)=>{
+module.exports.create = async (req,res)=>{
+    try{
+        let post = await Post.findById(req.body.post)
         if(post){
-            Comment.create({
+            let comment = await Comment.create({
                 content:req.body.content,
                 post:req.body.post,
                 user:req.user._id
-            })
-            .then((comment)=>{
-                post.comments.push(comment);
-                post.save();
-                
-                return res.redirect('/');
-            })
+            });
+            post.comments.push(comment);
+            post.save();
+            return res.redirect('/');
         }
-    })
+    }
+    catch(err){
+        console.log("Error",err);
+    }
 }
 
-module.exports.destroy = (req,res)=>{
-    Comment.findById(req.params.id)
-    .then((comment)=>{
-        if(comment.user == req.user.id){
-            let postId = comment.post;
-            Comment.findByIdAndDelete(req.user.id);
-            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}})
-            .then(()=>{
-                return res.redirect('back');
-            })
-        }
-    })
+module.exports.destroy = async (req,res)=>{
+    let comment = await Comment.findById(req.params.id)
+    if(comment.user == req.user.id){
+        let postId = comment.post;
+        await Comment.findByIdAndDelete(req.user.id);
+        await Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}})
+        return res.redirect('back');
+    }
 }
