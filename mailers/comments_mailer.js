4025cc251f93d2bd81a94bb9f3787a1d618dbc22
RaghavendraPatel <raghavpatel2808@hotmail.com>
2023-05-25 20:02:20 +0530

    added comment mailer

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,19 @@
+const nodemailer = require('../config/nodemailer');
+
+exports.newComment = (comment)=>{
+    console.log('inside new comment mailer',comment);
+    let htmlString = nodemailer.renderTemplate({comment:comment},'/comments/new_comment.ejs');
+    nodemailer.transporter.sendMail({
+        from:'raghavpatel2808.cn.test@gmail.com',
+        to: comment.user.email,
+        subject:'new comment published',
+        html:htmlString
+    },(err,info)=>{
+        if(err){
+            console.log('Error in sending mail',err);
+            return;
+        }
+        console.log('Message sent',info);
+        return;
+    })
+}
