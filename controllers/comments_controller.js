f7ebf79186339a16edc195a1ca817ec16414aca1
RaghavendraPatel <raghavpatel2808@hotmail.com>
2023-05-28 04:40:34 +0530

    add delayed jobs using kue

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,9 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
+
 module.exports.create = async (req,res)=>{
     try{
         let post = await Post.findById(req.body.post)
@@ -13,7 +16,14 @@ module.exports.create = async (req,res)=>{
             post.comments.push(comment);
             post.save();
             comment = await comment.populate('user', 'name email');
-            commentsMailer.newComment(comment);
+            // commentsMailer.newComment(comment);
+            let job = queue.create('emails',comment).save(function(err){
+                if(err){
+                    console.log("error in creating queue",err);
+                    return;
+                }
+                console.log('job enqueued',job.id);
+            });
             if (req.xhr){
                 return res.status(200).json({
                     data: {
--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -13,7 +13,7 @@ exports.newComment = (comment)=>{
             console.log('Error in sending mail',err);
             return;
         }
-        console.log('Message sent',info);
+        // console.log('Message sent',info);
         return;
     })
 }
